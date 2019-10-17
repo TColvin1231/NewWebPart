704350f2ea02826d9d2efc2d9bb9c7b67c26f7fe
TammyC1231 <tammycolvin@cloudapptestdev.onmicrosoft.com>
2019-10-17 11:37:31 -0500

    inital commit

---
+++ src/webparts/newWebPart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface INewWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'NewWebPartWebPartStrings' {
+  const strings: INewWebPartWebPartStrings;
+  export = strings;
+}
